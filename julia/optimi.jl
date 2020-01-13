3a71a3e427824da3231b007827020994275d2c81
yordiak <gi_di@hotmail.co.uk>
2020-01-13 14:09:17 +0200

    Add files via upload

---
+++ julia/optimi.jl
@@ -0,0 +1,45 @@
+using JuMP, Plots, Gurobi
+
+# Creating a function in order to improve speed and avoiding the 'global' argument of 'k'
+
+function run_model()
+
+    m=Model(with_optimizer(Gurobi.Optimizer))
+
+
+    @variable(m, x>=0)
+    @variable(m, y>=0)
+
+    @constraint(m, 2*x-4>=0)
+    @constraint(m, y-0.5*x>=0)
+
+
+    k=0 # iteration index
+    obj_val=Float64[] # storing objective values
+
+    while k<=1
+        φ(x,y,k)=3*x+k*y # objective function
+
+        @objective(m, Max, φ(x,y,k))
+    
+        optimize!(m)
+
+        push!(obj_val, objective_value(m))
+    
+        k +=0.2
+    end
+
+return obj_val
+
+end
+
+run_model() # generating data
+
+
+# Plotting the data
+
+k=[i for i in 0:0.2:1]
+
+gr() # blackened
+
+plot(k,obj_val)
