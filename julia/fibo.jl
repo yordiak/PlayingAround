3058b05a3e0d49c40096f88ff35083b3190ef634
yordiak <gi_di@hotmail.co.uk>
2020-02-14 15:26:28 +0200

    make the code in a more julian-way

--- julia/fibo.jl
+++ julia/fibo.jl
@@ -1,23 +1,25 @@
 # For any positive number n, determine the first N numbers of a Fibonacci sequence
 
-function fibo(n::Float64,N::Int64)
+function fibo(n::Any,N::Int64)
 
 f=0
-a=Float64[]
+    
+(typeof(n)==Int64) ? a=Int64[] : a=Flot64[] #specifying how the fibonacci sequence is presenting
 
-    for i=1:N
-        if i==1
-           push!(a,0)
-        elseif i==2
-            push!(a,n)
-        else
-            f=a[i-1]+a[i-2]
-            push!(a,f) 
-        end            
-    i= i+1
+for i=1:N
+    if i==1
+        push!(a,0)
+    elseif i==2
+        push!(a,n)
+    else
+        f=a[i-1]+a[i-2]
+        push!(a,f) 
     end
+        
+i= i+1
+        
+end
 
 return println("The fist $N terms of a Fibonacci sequence of the number $n is $a")
 
 end
- 
